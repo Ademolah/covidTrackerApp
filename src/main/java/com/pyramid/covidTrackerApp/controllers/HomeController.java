d93fd5a0ad273c8b3b3bdcb218335e174ea771a5
Charles <adeakinyemi129@gmail.com>
2021-05-11 22:13:27 +0300

    Initial commit

---
+++ src/main/java/com/pyramid/covidTrackerApp/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.pyramid.covidTrackerApp.controllers;
+
+import com.pyramid.covidTrackerApp.models.LocationStats;
+import com.pyramid.covidTrackerApp.services.CovidDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CovidDataService covidDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats>  allStats = covidDataService.getAllStats();
+        int totalReportedCases =  allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        return "home";
+    }
+}
---
+++ src/test/java/com/pyramid/covidTrackerApp/CovidTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyramid.covidTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
